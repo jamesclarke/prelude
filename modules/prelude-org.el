565b16046a0dc58701a55e6ad00b6f49728965bc
James Clarke <jclarke@touchtype-online.com>
2013-03-08 15:44:43 +0000

    Adding hook for .txt files in the org-mode configurations.

--- modules/prelude-org.el
+++ modules/prelude-org.el
@@ -33,6 +33,7 @@
 ;;; Code:
 
 (add-to-list 'auto-mode-alist '("\\.org\\’" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.txt\\’" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
